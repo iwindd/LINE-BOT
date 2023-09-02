3e21c043f0cf25fb36a69df04a1f17d5abc0bd0f
iwindd <freewinendlife@gmail.com>
2023-09-02 12:48:42 +0700

    express

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,1 +1,9 @@
-console.log("SYNTHIA..");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Synthia...")
+})
+
+app.listen(3000, () => console.log(`Server is running`))
